082391448ea975e2d26137eb9a2da72dc0e89b85
Omer Jonuzi <omerjonuzi7@gmail.com>
2020-10-20 10:47:47 +0200

    merge error demo 2

--- whiteboard/client/src/index.js
+++ whiteboard/client/src/index.js
@@ -7,6 +7,7 @@ import { Alert } from './widgets';
 //Hei
 //dette 
 //er en merge error
+//denne linja kommer til å kræsje
 const root = document.getElementById('root');
 if (root)
   ReactDOM.render(
