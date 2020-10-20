225b9e23dcdcda0d3be90bdcfcd72098602ae7c9
Omer Jonuzi <omerjonuzi7@gmail.com>
2020-10-20 10:32:16 +0200

    Tester merge error

--- whiteboard/client/src/index.js
+++ whiteboard/client/src/index.js
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom';
 import * as React from 'react';
 import { Whiteboard } from './whiteboard-component';
 import { Alert } from './widgets';
-
+//Hei
+//dette 
+//er en merge error
 const root = document.getElementById('root');
 if (root)
   ReactDOM.render(
