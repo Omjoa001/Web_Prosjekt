d8fb2d77cda2983f8d515b7524b6cba66fc18867
Torstein <tmaregge@gmail.com>
2020-10-22 14:14:30 +0200

    It now renders a dummy Quiz array

--- kazoot/client/src/kazoot-components.js
+++ kazoot/client/src/kazoot-components.js
@@ -3,7 +3,7 @@
 import * as React from 'react';
 import { Component } from 'react-simplified';
 import { Card, Row, Button, Form, Column, Alert } from './widgets';
-//import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
 import questionService from './kazoot-service';
 import { quizService, categoryService } from './kazoot-service';
 
@@ -32,11 +32,23 @@ export class App extends Component {
 
   mounted() {
     questionService.getAll().then((sprs) => (this.sprs = sprs));
-    quizService.hey(); // testing that the import works
   }
 }
 
 export class BrowseQuizzes extends Component {
+  // dummy quiz array
+  quizzes: Quiz[] = [
+    {
+      title: 'quiz 1',
+      id: 1,
+      category: 1,
+    },
+    {
+      title: 'quiz 2',
+      id: 2,
+      category: 2,
+    },
+  ];
   categories: number[] = [];
 
   render() {
@@ -44,11 +56,42 @@ export class BrowseQuizzes extends Component {
       <>
         <Card title="Categories">{this.categories}</Card>
         <Card title="Search"></Card>
+        <Card title="Quizzes">
+          {this.quizzes.map((quiz) => (
+            <Row key={quiz.id}>
+              <Column>
+                <Quiz id={quiz.id}></Quiz>
+                {/* <NavLink to={'/quizzes/' + quiz.id}>{quiz.title}</NavLink> */}
+              </Column>
+            </Row>
+          ))}
+        </Card>
       </>
     );
   }
 
   mounted() {
     categoryService.getAll().then((categories) => (this.categories = categories));
+  }
+}
+
+/**
+ * Quiz component.
+ * Should be called with an id.
+ */
+export class Quiz extends Component {
+  title: string = '';
+  id: number = 0;
+
+  render() {
+    return (
+      <>
+        <Card title={this.props.title}>{this.props.id}</Card>
+      </>
+    );
+  }
+
+  mounted() {
+    // quizService.get(this.props.id);
   }
 }
