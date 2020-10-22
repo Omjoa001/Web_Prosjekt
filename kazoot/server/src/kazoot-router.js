52eb3df3f244a057fb66d4c78ad6c8b3bda665cf
wilhelm <wilhelm.bjoland@online.no>
2020-10-22 11:29:29 +0200

    kjøøør da main

---
+++ kazoot/server/src/kazoot-router.js
@@ -0,0 +1,44 @@
+// @flow
+import express from 'express';
+import quizService, { type Sprs } from './kazoot-service';
+
+/**
+ * Express router containing task methods.
+ */
+const router: express$Router<> = express.Router();
+
+router.get('/tasks', (request, response) => {
+  quizService
+    .getAll()
+    .then((rows) => response.send(rows))
+    .catch((error: Error) => response.status(500).send(error));
+});
+
+router.get('/tasks/:id', (request, response) => {
+  const id = Number(request.params.id);
+  quizService
+    .get(id)
+    .then((task) => (task ? response.send(task) : response.status(404).send('Task not found')))
+    .catch((error: Error) => response.status(500).send(error));
+});
+
+// Example request body: { title: "Ny oppgave" }
+// Example response body: { id: 4 }
+router.post('/tasks', (request, response) => {
+  const data = request.body;
+  if (data && typeof data.title == 'string' && data.title.length != 0)
+    quizService
+      .create(data.title)
+      .then((id) => response.send({ id: id }))
+      .catch((error: Error) => response.status(500).send(error));
+  else response.status(400).send('Missing task title');
+});
+
+router.delete('/tasks/:id', (request, response) => {
+  quizService
+    .delete(Number(request.params.id))
+    .then((result) => response.send())
+    .catch((error: Error) => response.status(500).send(error));
+});
+
+export default router;
