4a3638e870643cbf2d6934c5afb6f24fe2a82ad9
Omer Jonuzi <omerjonuzi7@gmail.com>
2020-11-05 13:39:46 +0100

    Blå edit knapp

--- kazoot/client/src/browsequizzes-components.js
+++ kazoot/client/src/browsequizzes-components.js
@@ -306,7 +306,7 @@ export class Quiz extends Component {
               </Button.Success>
             </Column>
             <Column right>
-            <Button.Success onClick={() => history.push('/editQuiz')}>Edit</Button.Success>
+            <Button.Primary onClick={() => history.push('/editQuiz')}>Edit</Button.Primary>
             </Column>
           </Row>
         </TileCard>
