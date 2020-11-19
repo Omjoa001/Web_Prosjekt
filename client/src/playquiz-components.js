d67c75a9efe414d8860abfb41b9313dea91d9739
Torstein <tmaregge@gmail.com>
2020-11-19 21:27:14 +0100

    Removed stuff

--- client/src/playquiz-components.js
+++ client/src/playquiz-components.js
@@ -87,23 +87,6 @@ export class PlayQuiz extends Component {
     return array;
   }
 
-  points: number = 0;
-
-  selectStyle = [{}, {}, {}, {}];
-  //Funksjon som plusser på en variabel, som blir pusha til en array for hver button. Da har vi en liste med unik identifikator på hver svar. Det kan stil sjekke før den setter verdi, og onclick flipper verdien.
-  stil(num: number, corr: boolean, style: boolean) {
-    style = !style;
-    if (corr) {
-      console.log('haei');
-      this.points++;
-    }
-    for (let i = 1; i <= this.selectStyle.length; i++) {
-      if (num == i) {
-        this.selectStyle[i - 1] = { border: '3px solid navy' };
-      }
-    }
-  }
-  styleBools = [];
   render() {
     return (
       <>
@@ -112,82 +95,11 @@ export class PlayQuiz extends Component {
           <br></br>
           <br></br>
         </CenterCard>
-
-        <div>
-          {this.shuffledQuestions.map((a) => (
-            <div key={a.id}>
-              <AnswerCard title={a.question}>
-                <div title={a.question}>
-                  Correct answers: {a.numCorrect}
-                  <br></br>
-                </div>
-                <Button.Answer
-                  onClick={this.stil.bind(this, 1, a.answ0[1])}
-                  style={a.answ0[2] ? { border: '3px solid navy' } : { border: '3px solid red' }}
-                  correct={a.answ0[1]}
-                  show={this.show}
-                >
-                  {a.answ0[0]}
-                </Button.Answer>
-                <Button.Answer
-                  style={a.answ1[2] ? { border: '3px solid navy' } : { border: '3px solid red' }}
-                  onClick={this.stil.bind(this, 2, a.answ1[1])}
-                  correct={a.answ1[1]}
-                  show={this.show}
-                >
-                  {a.answ1[0]}
-                </Button.Answer>
-                <Button.Answer
-                  style={this.selectStyle[2]}
-                  onClick={this.stil.bind(this, 3, a.answ2[1])}
-                  correct={a.answ2[1]}
-                  show={this.show}
-                >
-                  {a.answ2[0]}
-                </Button.Answer>
-                <Button.Answer
-                  style={this.selectStyle[3]}
-                  onClick={this.stil.bind(this, 4, a.answ3[1])}
-                  correct={a.answ3[1]}
-                  show={this.show}
-                >
-                  {a.answ3[0]}
-                </Button.Answer>
-              </AnswerCard>
-              <br></br>
-              <br></br>
-            </div>
-          ))}
-        </div>
-
-        <center>
-          <Button.Submit
-            onClick={() => {
-              this.show = true;
-              this.resultText = `You got ${this.points}/${this.totalPoints} correct answers`;
-            }}
-          >
-            SUBMIT ANSWERS 🎉
-          </Button.Submit>
-          <br></br>
-          <Card title={this.resultText}></Card>
-        </center>
-        <br></br>
-        <br></br>
-        <br></br>
       </>
     );
   }
+
   mounted() {
     this.id = this.props.match.params.id;
-    //quizService.getNextId().then((next) => (this.nextId = next.AUTO_INCREMENT));
-    quizService.getQuiz(this.id).then((q) => (this.quiz = q));
-    questionService
-      .getQuizQuestion(this.id)
-      .then((p) => (this.questions = p))
-      .then(() => {
-        this.randomizeOrder(this.questions);
-      });
-    categoryService.getAllCategories().then((c) => (this.categories = c));
   }
 }
